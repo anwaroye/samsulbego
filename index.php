1e1fcd95d919e040d6f9c61dd2454033fa9c8bf1
anwaroye <anwaroye68@gmail.com>
2018-07-18 00:13:45 +0700

    kontol

---
+++ index.php
@@ -0,0 +1,365 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Ekstrakulikuler SMAN1 JEPANG</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="bs4/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="bs4/css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="bs4/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="bs4/css/bootstrap.css">
+
+    <!-- Custom fonts for this template -->
+    <script type="text/javascript" src="bs4/js/bootstrap.js"></script>
+
+    <script type="text/javascript" src="bs4/js/bootstrap.bundle.js"></script>
+    <script type="text/javascript" src="bs4/js/bootstrap.bundle.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+    <link href="css/grayscale.css" rel="stylesheet">
+
+    
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">SMA Negeri Ikhe Ikhe Japan</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#masthead">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#berita">Berita</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#eskul">Ekstrakulikuler</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#latedNews">kegiatan</a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+          <h1 class="mx-auto my-0 text-uppercase">OSIS Web</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">Web Ini Berisi Tentang Ekstrakulikuler SMA Negeri IKhe Ikhe Japan</h2>
+          <a href="#apa" class="btn btn-primary js-scroll-trigger">Get Started</a>
+        </div>
+      </div>
+    </header>
+
+     <!-- Page thumail lates news -->
+     <section id="berita" class="latedNews ">
+     <div class="container"  id="ltnews">
+      <div  class="TitleSection">
+        <h3>LATES NEWS</h3>
+      </div>
+      <div class="row">
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                <p>
+                  <a href="#" class="btn btn-info btn-xs" role="button">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+        <div class="btnReadMore">
+          <a href="news.php" class="btn btn-info btn-large waves-effect white center-block">LEBIH BANYAK</a></div>
+    </section>
+
+    <script type="text/javascript">
+      $('.thumbnail').hover(
+        function(){
+          $(this).find('.caption').slideDown(250);
+
+        },
+        function(){
+        $(this).find('.caption').slideUp(250);
+      }
+      );
+    </script>
+
+
+<!-- section beberapa eskul -->
+    <section id="eskul" class="sectionEskul">
+      <div class="container" id="container-eskul">
+        <div class="titleEskul">
+          <h3>
+            EKSTRAKULIKULER
+          </h3>
+        </div>
+        <div class="text-uppercase" id="introEskul">
+          <h6>
+            Ekstrakulikuler Di SMAN 1 Ikhe Ikhe Japan Sebanyak 69 
+          </h6>
+        </div>
+          <div class="row">
+
+            <!-- =========eskul=================================== -->  
+             <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail" id="thumbnailEskul">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail" id="thumbnailEskul">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail" id="thumbnailEskul">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail" id="thumbnailEskul">
+            <img src="img/thumbnail.jpg" alt="">
+              <div class="caption">
+            </div>
+          </div>
+        </div>
+
+          </div>
+        </div>  
+         <div class="btbReadEskul">
+          <a href="news.php" class="btn btn-info btn-large waves-effect white center-block">LEBIH BANYAK</a></div>  
+    </section>
+    <!-- end section -->
+    <section id="sectionPoster">
+      <div class="container" id="containerPoster">
+        <header class="headerPoster">
+        <div class="TitlePoster">
+          <h3>Galeri Poster</h3>
+          <h3 class="text-uppercase bold"> poster kegiatan atau acara</h3>
+          <h5>Baik Acara Formal Maupun  Non-Formal</h5>
+        </div>
+      </header>
+        <div class="row">
+
+
+          <div class="col-xs-18 col-sm-6 col-md-4">
+          <div class="thumbnailPoster" id="poster">
+            <img src="img/wandri2.png">
+          </div>
+          </div>
+
+
+
+          <div class="col-xs-18 col-sm-6 col-md-4">
+          <div class="thumbnailPoster" id="poster">
+            <img src="img/wandri2.png">
+          </div>
+          </div>
+
+
+          <div class="col-xs-18 col-sm-6 col-md-4">
+          <div class="thumbnailPoster" id="poster">
+            <img src="img/wandri2.png">
+          </div>
+          </div>
+
+
+          <div class="col-xs-18 col-sm-6 col-md-4">
+          <div class="thumbnailPoster" id="poster">
+            <img src="img/wandri2.png">
+          </div>
+          </div>
+
+
+          <div class="col-xs-18 col-sm-6 col-md-4">
+          <div class="thumbnailPoster" id="poster">
+            <img src="img/wandri2.png">
+          </div>
+          </div>
+
+
+          <div class="col-xs-18 col-sm-6 col-md-4">
+          <div class="thumbnailPoster" id="poster">
+            <img src="img/wandri2.png">
+          </div>
+          </div>
+
+    </div>
+      </div>
+    </section>
+    <section class="sectionLeader">
+      <div class="container">
+        <div class="leader">
+           <h4>leader</h4>
+          </div>
+        <div class="row" id="rowLeader">
+          <div class="col-xl-4">
+           <figure class="leader">
+              <img src="img/wandri2.png">
+              <figcaption>
+              <h4>Maulana Arif R.</h4>
+              <p>Ketua 1</p>
+              <div class="meta-social">
+                <ul class="list-inline">
+                  <li><a href="https://www.instagram.com/maulanariff/" target="_blank" class="instagram-share border-box"><i class="fa fa-clipboard fa-lg"></i></a></li>
+                  
+                  <li><a href="https://line.me/R/ti/p/@ariff00maul" target="_blank" class="line-share border-box"><i class="fa fa-comment fa-lg"></i></a></li>
+                </ul>
+              </div>
+            </figcaption>
+            </figure>
+          
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+      <div class="container">
+        Copyright &copy; Your Website 2018
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/font-awesome/js/all.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/grayscale.min.js"></script>
+
+
+
+  </body>
+
+</html>
